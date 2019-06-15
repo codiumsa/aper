ff582f7f0822cf7675523180977bbb7ccd77053e
javierpf <javierppf@gmail.com>
2019-06-15 09:10:27 -0400

    Initial project

---
+++ README.md
@@ -0,0 +1,10 @@
+#### Suggest a better name, or Aper
+
+### `Setup your environment`
+
+cd into the project root. Create `env.development.local` file initializing REACT_APP_GOOGLE_CID to your Google Client ID
+
+### `Install and start dev mode`
+
+- `yarn install`
+- `yarn start`
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'typeface-poppins';
+import { Authenticator } from './components/Authenticator';
+import Login from './components/Login';
+import Home from './components/Home';
+import { CustomThemeProvider } from './theme';
+import './utils/i18n';
+
+function App() {
+  return (
+    <Router>
+      <CustomThemeProvider>
+        <CssBaseline />
+        <Authenticator>
+          <Route path={'/login'} component={Login} />
+          <Route path={'/home'} component={Home} />
+        </Authenticator>
+      </CustomThemeProvider>
+    </Router>
+  );
+}
+
+export default App;
